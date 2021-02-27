6b2a06a711e6429171479a765ea2a894a920063a
Alifia Zaida Nurmaya <70618498+AlifiaZaidaNurmaya@users.noreply.github.com>
2021-02-27 14:26:30 +0700

    push

---
+++ Modul2/slider/Assets/SliderValueToText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Slider value = " +
+        sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
